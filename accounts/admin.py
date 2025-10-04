986a879c6e285a93d64e0d1fb12c9ec3715679f2
JohnDeni <johndeni2002@gmail.com>
2025-10-04 10:35:11 +0300

    Add custom User model and apply initial migrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# accounts/admin.py
+from django.contrib import admin
+from .models import User, UserProfile, Seller
+
+admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Seller)

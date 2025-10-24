5543edbdd7dea450f84cc2e10808dcc025521a3e
JohnDeni <johndeni2002@gmail.com>
2025-10-24 12:27:21 +0300

    Update admin configuration for Seller and UserProfile models

--- accounts/models.py
+++ accounts/models.py
@@ -39,12 +39,26 @@ class UserProfile(Audit):
     # pip install "django-phonenumber-field[phonenumberslite]"
     phone_number = PhoneNumberField()
     role = models.CharField(max_length=7, choices=ROLE_CHOICES, default="buyer")
-    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name="profile")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 
 class Seller(Audit, models.Model):
     company_name = models.CharField(max_length=50)
     phone_number = PhoneNumberField()
     address = models.TextField()
     user = models.ManyToManyField(User, related_name="seller", blank=True)
+
+    def __str__(self):
+        return (f'Name company: {self.company_name}'
+                f'Phone number: {self.phone_number}'
+                f'Address: {self.address}')
+    # class Meta:
+    #     ordering = ['-created_at']
+    #     verbose_name_plural = "sellers"
+    #     permissions = ()
 
 

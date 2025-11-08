0226d1af778c9a7ad4c0b4bc8d8315da66b19ecb
darynarodicheva <rdchvd29@gmail.com>
2025-11-08 13:58:23 +0200

    feat(LD-140): Add swagger

--- sparehub/urls.py
+++ sparehub/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from accounts.urls import router as accounts_router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/accounts/", include(accounts_router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]

904a3e13ecf8eae1eb95aca61c6812572d5f6bbf
JohnDeni <johndeni2002@gmail.com>
2026-02-20 16:24:36 +0200

    Implemented delete method

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.utils import timezone
 from rest_framework import mixins, status, viewsets
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 
 from accounts.models import UserProfile
@@ -23,31 +22,15 @@ class UserProfileViewSet(
     def destroy(self, request, *args, **kwargs):
         profile = self.get_object()
         user_to_delete = profile.user
-        request_user = request.user
 
-        if request_user.is_staff:
-            user_to_delete.is_active = False
-            user_to_delete.save()
+        user_to_delete.is_active = False
+        user_to_delete.deleted_at = timezone.now()
+        user_to_delete.save()
 
-            profile.deleted_at = timezone.now()
-            profile.save()
+        profile.deleted_at = timezone.now()
+        profile.save()
 
-            return Response(
-                {"detail": f"User {user_to_delete.username} has been deactivated."},
-                status=status.HTTP_200_OK,
-            )
-
-        if request_user == user_to_delete:
-            user_to_delete.is_active = False
-            user_to_delete.deleted_at = timezone.now()
-            user_to_delete.save()
-
-            profile.deleted_at = timezone.now()
-            profile.save()
-
-            return Response(
-                {"detail": "Your account has been deactivated."},
-                status=status.HTTP_200_OK,
-            )
-
-        raise PermissionDenied("You can only delete your own account.")
+        return Response(
+            {"detail": f"User {user_to_delete.email} has been deactivated."},
+            status=status.HTTP_200_OK,
+        )

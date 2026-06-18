8f3d437033041be1ff4eda6e06d4401d805d0451
JohnDeni <johndeni2002@gmail.com>
2026-06-18 12:42:08 +0300

    LD-166 fix negative price validation for product creation

--- products/serializers.py
+++ products/serializers.py
@@ -5,6 +5,8 @@ from products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
 
+    price = serializers.DecimalField(max_digits=10, decimal_places=2, min_value=0)
+
     class Meta:
         model = Product
         fields = "__all__"
